b8088a6e3ef8a220eb9d0921508d047df7105549
Isaac Radford <radf.isaac@gmail.com>
2024-02-06 01:43:39 -0700

    Updated home page animations

--- client/personal_portfolio/src/pages/HomePage.js
+++ client/personal_portfolio/src/pages/HomePage.js
@@ -14,6 +14,7 @@ export const HomePage = () => {
 	const [projects, setProjects] = useState([]);
 	const [backgroundColor, setBackgroundColor] = useState("#f3f3f3");
 	const [animationColor, setAnimationColor] = useState("#000");
+	const [isLoading, setIsLoading] = useState(false);
 	const api_password = process.env.REACT_APP_API_PASSWORD;
 
 	// Remove previous body classes
@@ -22,6 +23,7 @@ export const HomePage = () => {
 	// Makes call to backend to get the list of all projects
 	useEffect(() => {
 		const getProjectList = () => {
+			setIsLoading(true);
 			const options = {
 				method: "GET",
 				url: "https://portfolio-api-mjjt.onrender.com/projects",
@@ -37,22 +39,18 @@ export const HomePage = () => {
 				})
 				.catch(function (error) {
 					console.error(error);
+				})
+				.finally(() => {
+					setIsLoading(false);
 				});
 		};
 		getProjectList();
 	}, []);
 
-	const LoadAnimations = () => {
-		onLoadHomeProjectAnimation();
-		onLoadIntroAnimation();
-	  };
-
-
 	useEffect(() => {
 		// Make sure to remove previous classes
 		document.body.classList = "";
-
-		document.body.style.overflow = 'hidden';
+		// document.body.style.overflowX = 'hidden';
 		document.documentElement.style.setProperty("--background-color", backgroundColor);
 		document.documentElement.style.setProperty("--animation-color", animationColor);
 		document.documentElement.style.setProperty('--text-color-home', '#f3f3f3');
@@ -65,80 +63,146 @@ export const HomePage = () => {
 			item.classList.add("HeaderNavHomeLoad");
 		})
 
-		LoadAnimations();
-	}, [backgroundColor, animationColor, LoadAnimations])
+	}, [backgroundColor, animationColor])
+	
+	useEffect(() => {
+        if (!isLoading) {
+            LoadAnimations();
+        }
+    }, [isLoading]);
+
+	const LoadAnimations = () => {
+		onLoadHomeProjectAnimation();
+		onLoadIntroAnimation();
+	  };
 
 	const handleColorChange = (newColor, newAnimationColor) => {
 		setBackgroundColor(newColor);
 		setAnimationColor(newAnimationColor);
 	  };
 
-	return (
-		<div className="homePage">
-			<div className="homeArea" >
-                <ul className="homeCircles">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                </ul>
-            </div >
+	  return (
+        <div className="homePage">
+            {isLoading ? (
+				<>
+					{/* <HeaderNav /> */}
+					{/* <Logo loadAnimation={true} /> */}
+					{/* <div className="homeArea">
+                        <ul className="homeCircles">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div> */}
 
-			<HeaderNav />
-			{/* <SocialLinks /> */}
-			<Logo loadAnimation={true}/>
-			<div className="projectBox">
-				<ul className="projects">
-				{/* <li className="projectLi"><div className="projectContainer"><a className="projectTitle projectStuffing">a</a></div></li> */}
+					<div className="loadingAnimation">
+						<h1 className="lds-title">Loading, please wait</h1>
+						<div className="lds-ellipsis">
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+						</div>
+					</div>
 
-					{projects.map((project, index) => {
-						return <ProjectNavList key={index} project={project}/>;
-					})}
-			
-				</ul>
-			</div>
 
-			<div className="colorBox">
-				<h1 className="clickMe">Click me:</h1>
-				<div className="colorButtons" id="radios">
-					<label for="input1"></label>
-					<input  className="colorButton1" 
-							id="input1"
-							defaultChecked
-							name="colorButton"
-							type="radio" 
-							onChange={() => handleColorChange("#f3f3f3", "#15161d")} />
-				
-					<label for="input2"></label>
-					<input  className="colorButton2" 
-							id="input2"
-							name="colorButton"
-							type="radio" 
-							onChange={() => handleColorChange("#15161d", "#f3f3f3")} />
+					<div className="colorBox">
+                        <h1 className="clickMe">Click me:</h1>
+                        <div className="colorButtons" id="radios">
+                            <label for="input1"></label>
+                            <input className="colorButton1"
+                                id="input1"
+                                defaultChecked
+                                name="colorButton"
+                                type="radio"
+                                onChange={() => handleColorChange("#f3f3f3", "#15161d")} />
 
-					<label for="input3"></label>
-					<input  className="colorButton3" 
-							id="input3"
-							name="colorButton"
-							type="radio" 
-							onChange={() => handleColorChange("#1B4332", "#74C69D")} />
+                            <label for="input2"></label>
+                            <input className="colorButton2"
+                                id="input2"
+                                name="colorButton"
+                                type="radio"
+                                onChange={() => handleColorChange("#15161d", "#f3f3f3")} />
 
-					<label for="input4"></label>
-					<input  className="colorButton4" 
-							id="input4"
-							name="colorButton"
-							type="radio" 
-							onChange={() => handleColorChange("#2B2D42", "#3543D4")} />
+                            <label for="input3"></label>
+                            <input className="colorButton3"
+                                id="input3"
+                                name="colorButton"
+                                type="radio"
+                                onChange={() => handleColorChange("#1B4332", "#74C69D")} />
 
-					<span id="slider"></span>
-				</div>
-			</div>
-		</div>
-	);
+                            <label for="input4"></label>
+                            <input className="colorButton4"
+                                id="input4"
+                                name="colorButton"
+                                type="radio"
+                                onChange={() => handleColorChange("#2B2D42", "#3543D4")} />
+
+                            <span id="slider"></span>
+                        </div>
+                    </div>
+				</>
+            ) : (
+                // Display content when isLoading is false
+                <>
+				<div className="animated-bg"></div>
+				<div className="animated-bg2"></div>
+				<div className="animated-bg3"></div>
+
+                    <HeaderNav />
+                    <Logo loadAnimation={true} />
+                    <div className="projectBox">
+                        <ul className="projects">
+                            {projects.map((project, index) => {
+                                return <ProjectNavList key={index} project={project} />;
+                            })}
+                        </ul>
+                    </div>
+
+                    <div className="colorBox">
+                        <h1 className="clickMe">Click me:</h1>
+                        <div className="colorButtons" id="radios">
+                            <label for="input1"></label>
+                            <input className="colorButton1"
+                                id="input1"
+                                defaultChecked
+                                name="colorButton"
+                                type="radio"
+                                onChange={() => handleColorChange("#f3f3f3", "#15161d")} />
+
+                            <label for="input2"></label>
+                            <input className="colorButton2"
+                                id="input2"
+                                name="colorButton"
+                                type="radio"
+                                onChange={() => handleColorChange("#15161d", "#f3f3f3")} />
+
+                            <label for="input3"></label>
+                            <input className="colorButton3"
+                                id="input3"
+                                name="colorButton"
+                                type="radio"
+                                onChange={() => handleColorChange("#1B4332", "#74C69D")} />
+
+                            <label for="input4"></label>
+                            <input className="colorButton4"
+                                id="input4"
+                                name="colorButton"
+                                type="radio"
+                                onChange={() => handleColorChange("#474bff", "#3543D4")} />
+
+                            <span id="slider"></span>
+                        </div>
+                    </div>
+                </>
+            )}
+        </div>
+    );
 }
