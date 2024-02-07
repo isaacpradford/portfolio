ed36e321d9df1c8fd29fe3fbbca7340f4a0a103d
Isaac Radford <radf.isaac@gmail.com>
2024-02-07 15:54:20 -0700

    Updated names

--- client/personal_portfolio/src/components/Logo.js
+++ client/personal_portfolio/src/components/Logo.js
@@ -9,8 +9,8 @@ export default function Logo({loadAnimation})
         return (
             <div className="introBox">
                 {/* <img className={`logo ${logoClass}`} src={logo} alt="Logo for website"/> */}
-                <h1 className={`logo ${logoClass}`}>IR</h1>
-                <h1 className="introName">Isaac Radford</h1>
+                <h1 className={`logo ${logoClass}`}>PR</h1>
+                <h1 className="introName">Parker Radford</h1>
                 <h2 className="introDegree">Web Development / Software Design</h2>
             </div>
         )
@@ -19,7 +19,7 @@ export default function Logo({loadAnimation})
             <div className="introBox">
                 <Link to="/" className="logo-link">
                     {/* <img className={`logo ${logoClass}`} src={logo} alt="Logo for website"/> */}
-                    <h1 className={`logo ${logoClass}`}>IR</h1>
+                    <h1 className={`logo ${logoClass}`}>PR</h1>
                 </Link>
             </div>
         )
