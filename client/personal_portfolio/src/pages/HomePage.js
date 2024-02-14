4dfc3898af5b349017059f4e76f2cd8cd11b6d0d
Isaac Radford <radf.isaac@gmail.com>
2024-02-13 17:17:23 -0700

    Added load time disclaimer and fixed small visual bugs

--- client/personal_portfolio/src/pages/HomePage.js
+++ client/personal_portfolio/src/pages/HomePage.js
@@ -111,6 +111,8 @@ export const HomePage = () => {
 							<div></div>
 							<div></div>
 						</div>
+
+                        <p className="loading-disclaimer">This website is currently hosted using the free tiers for hosting, resulting in longer load times and some visual bugs. <br></br><br></br>Apologies in advance, and thank you for your patience!</p>
 					</div>
 
 
