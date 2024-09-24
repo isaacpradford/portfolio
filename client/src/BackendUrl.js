4292688c9524d2b6a0e7f35ef30d8bba3e1842c4
isaacpradford <radf.isaac@gmail.com>
2024-09-23 22:07:13 -0500

    Updated URLs for server

--- client/src/BackendUrl.js
+++ client/src/BackendUrl.js
@@ -1,8 +1,8 @@
 const state = process.env.REACT_APP_STATE;
 export var backendUrl;
 
-if (state === "DEVELOPMENT") {
-  backendUrl = "https://iradford.onrender.com/"; // You'll need to fix this lol
+if (state === "PRODUCTION") {
+  backendUrl = "https://iradford-server.onrender.com/"; // You'll need to fix this lol
 } else if (state === "LOCAL") {
   backendUrl = "http://localhost:5500/";
 } else {
