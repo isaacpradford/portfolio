f4738dbb600f43036e31ce91dadfa95410ab9439
Isaac Radford <radf.isaac@gmail.com>
2024-10-21 10:42:06 -0500

    Updated home styling juussst a tad

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,11 +2,7 @@ import React, { useRef } from "react";
 import { FaRegMoon } from "react-icons/fa6";
 import { GiCobweb, GiMoonBats } from "react-icons/gi";
 import { Link } from "react-scroll";
-import {
-  motion,
-  useScroll,
-  useTransform,
-} from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 import Socials from "../Components/Sections/Socials";
 
@@ -33,14 +29,14 @@ const HomePage = () => {
 
   return (
     <motion.div id="home" className="b-home page" ref={ref}>
-      <motion.div
+      {/* <motion.div
         className="b-home__square1"
         style={{ translateX: leftBoxTranslateX }}
       ></motion.div>
       <motion.div
         className="b-home__square2"
         style={{ translateX: rightBoxTranslateX }}
-      ></motion.div>
+      ></motion.div> */}
 
       <motion.div className="b-home__title">
         <Link to="home">
@@ -48,8 +44,8 @@ const HomePage = () => {
           <h1>+</h1>
           <h1>Full Stack WebDev</h1>
         </Link>
+        <p className="b-home__name">Isaac Radford</p>
       </motion.div>
-      <h1 className="b-home__name">Isaac Radford</h1>
 
       <motion.div className="b-home__socials">
         <Socials />
